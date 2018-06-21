5621712f458c07ad00d8e235b7eee1c0581a62a4
Jason Swett <jason@benfranklinlabs.com>
2018-06-20 21:58:25 -0400

    Add specs for Genus.

--- spec/models/genus_spec.rb
+++ spec/models/genus_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Genus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'name is missing' do
+    it 'is not valid' do
+      genus = Genus.new(name: nil)
+
+      expect(genus).not_to be_valid
+    end
+  end
+
+  context 'name is not unique' do
+    it 'is not valid' do
+      other_genus = Genus.create!(name: 'Bison')
+      genus = Genus.new(name: 'Bison')
+
+      expect(genus).not_to be_valid
+    end
+  end
 end
